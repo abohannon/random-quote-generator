0cd978c91907c63272ba4915a1723dda23a7cf70
Adam Bohannon <abo46n2@gmail.com>
2017-07-21 23:15:05 -0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#e67e22', 
    var quotesList =  $.getJSON("https://gist.githubusercontent.com/abohannon/a0187309c729f04d571b74cfca893dac/raw/e680af4b71494e520c4d6f8bfaa2e98142e728fe/trump-quote2",
     function(json){
     i = Math.floor(Math.random() * (41)); // Randomize JSON items
+      console.log(i)
 
           // Variables referencing JSON feed key/value pairs
           currentQuote = json[i].quote;
@@ -46,7 +47,7 @@ var colors = ['#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#34495e', '#e67e22', 
       $("#tweet-quote").attr('href', 'http://twitter.com/share?text='+'"'+currentQuote+'"'+ " - " +currentAuthor+'&hashtags=shittrumpsaid');
 
       var color = Math.floor(Math.random() * colors.length); // Randomize colors from colors array at top
-      console.log("color is" + " " + color); // Check if function is returning a numerical value for the color array
+      
 
       // JQuery animations for colors
       $("html body").animate(
